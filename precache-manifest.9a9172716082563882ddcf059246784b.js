89ec1d4ec448b839ea61643e02b09151e37fe7af
jwkevinc <jwkevinc@gmail.com>
2020-03-04 16:24:42 -0500

    Updates

---
+++ precache-manifest.9a9172716082563882ddcf059246784b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d354a76535622c805085aabbf162e8f5",
+    "url": "/konva-react/index.html"
+  },
+  {
+    "revision": "044e19ade9ead482f91b",
+    "url": "/konva-react/static/css/main.d473f19c.chunk.css"
+  },
+  {
+    "revision": "2f82b45d328451bed94a",
+    "url": "/konva-react/static/js/2.de9efacb.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/konva-react/static/js/2.de9efacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044e19ade9ead482f91b",
+    "url": "/konva-react/static/js/main.9475d60f.chunk.js"
+  },
+  {
+    "revision": "69e6eb82bb2a9d0785c9",
+    "url": "/konva-react/static/js/runtime-main.e800e16b.js"
+  }
+]);
